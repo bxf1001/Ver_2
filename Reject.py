7cd35131d690969f7e059e9a3fcc8e743af41ec6
bxf1001 <149566631+bxf1001@users.noreply.github.com>
2024-02-16 12:30:24 +0530

    Update Reject.py

--- Reject.py
+++ Reject.py
@@ -1,12 +1,11 @@
 from pywinauto import Application
 from pywinauto.keyboard import send_keys
+import warning_label
 import time
 app = Application(backend='uia').start(r"cmd.exe /c start shell:appsFolder\5319275A.WhatsAppDesktop_cv1g1gvanyjgm!App", create_new_console=True, wait_for_idle=False)
-time.sleep(2)
+
 app = Application(backend='uia').connect(title_re="WhatsApp")
-time.sleep(2)
 dialog = app.window(title="Group video call \u200e- WhatsApp")
-time.sleep(2)
 button = dialog.child_window(title="Group video call ‎- WhatsAApp", auto_id="TitleBar", control_type="Window")
 #app.dialog.print_control_identifiers()
 while True:
@@ -14,6 +13,8 @@ while True:
         if button.is_enabled():
             time.sleep(2)
             app.Dialog.child_window(title="End call", auto_id="EndCallButton", control_type="Button").click()
+            warning_label.show_warning()
+            time.sleep(2)
             send_keys("{VK_F12}")
             break  # Button is enabled, so click it and exit the loop
 
